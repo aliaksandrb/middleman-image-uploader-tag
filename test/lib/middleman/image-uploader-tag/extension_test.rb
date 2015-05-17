c1b7ac380d9d5195fc9fbd4a3be5d6cc4a9bca3a
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-05-18 00:45:13 +0300

    Get rid of instance variables from tests

--- test/lib/middleman/image-uploader-tag/extension_test.rb
+++ test/lib/middleman/image-uploader-tag/extension_test.rb
@@ -1,6 +1,8 @@
 require './test/test_helper'
 
 class ExtensionTest < Minitest::Test
+  attr_reader :application, :ext_instance, :ext_class
+
   def setup
     @application = Class.new(Middleman::Application)
     @ext_instance = Middleman::ImageUploaderTag::Extension.new(@application)
@@ -12,30 +14,30 @@ class ExtensionTest < Minitest::Test
   end
 
   def test_app
-    assert_equal @application, @ext_class.app
+    assert_equal application, ext_class.app
   end
 
   def test_provider_options
-    assert_equal @ext_instance.options, @ext_class.provider_options
+    assert_equal ext_instance.options, ext_class.provider_options
   end
 
   def test_remote_images_dir
-    set_provider_options @ext_class, { remote_images_dir: 'test_folder' }
-    assert_equal 'test_folder', @ext_class.remote_images_dir
+    set_provider_options ext_class, { remote_images_dir: 'test_folder' }
+    assert_equal 'test_folder', ext_class.remote_images_dir
 
-    set_provider_options @ext_class, { remote_images_dir: nil }
-    assert_equal 'remote_images', @ext_class.remote_images_dir
+    set_provider_options ext_class, { remote_images_dir: nil }
+    assert_equal 'remote_images', ext_class.remote_images_dir
   end
 
   def test_create_images_dir!
-    remote_images_dir = File.join(@application.root, 'source', @ext_class.remote_images_dir)
+    remote_images_dir = File.join(application.root, 'source', ext_class.remote_images_dir)
 
     assert Dir.exists?(remote_images_dir)
 
     Dir.rmdir(remote_images_dir)
     refute Dir.exists?(remote_images_dir)
 
-    @ext_class.create_images_dir!
+    ext_class.create_images_dir!
     assert Dir.exists?(remote_images_dir)
   end
 
@@ -44,11 +46,11 @@ class ExtensionTest < Minitest::Test
   end
 
   def test_image_location
-    remote_images_dir = File.join(@application.root, 'source', @ext_class.remote_images_dir)
+    remote_images_dir = File.join(application.root, 'source', ext_class.remote_images_dir)
 
-    assert_equal remote_images_dir + '/test.jpg', @ext_class.image_location('test.jpg')
-    assert_equal remote_images_dir + '/test.jpg', @ext_class.image_location('/test.jpg')
-    assert_equal remote_images_dir + '/test/test.jpg', @ext_class.image_location('test/test.jpg')
+    assert_equal remote_images_dir + '/test.jpg', ext_class.image_location('test.jpg')
+    assert_equal remote_images_dir + '/test.jpg', ext_class.image_location('/test.jpg')
+    assert_equal remote_images_dir + '/test/test.jpg', ext_class.image_location('test/test.jpg')
   end
 
   def test_provider
