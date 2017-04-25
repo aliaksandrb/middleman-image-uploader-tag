be91eea92372f8937e8ad9947bd7e2402b5d3a49
Harry Whelchel <harry@standardcraft.io>
2017-04-24 21:57:36 -0400

    use config[:images_dir]
    temporary logging
    
    reference logger on app
    
    use production env
    
    use config mode
    
    removes logging

--- lib/middleman/image-uploader-tag/extension.rb
+++ lib/middleman/image-uploader-tag/extension.rb
@@ -37,7 +37,7 @@ module Middleman
       end
 
       def self.image_location(image_path)
-        File.join(app.root, 'source', app.images_dir, remote_images_dir, image_path)
+        File.join(app.root, 'source', app.config[:images_dir], remote_images_dir, image_path)
       end
 
       def self.provider
@@ -51,16 +51,17 @@ module Middleman
 
       def self.get_remote_path(image_name, secure = false)
         image_path = image_location(image_name)
+
         raise NotFound unless File.exist?(image_path)
 
-        if app.config.environment == :build
+        if app.config[:mode] == :build
           if provider.instance_of? ::Middleman::ImageUploaderTag::CloudinaryCDN
             provider.get_remote_link image_path, secure
           else
             provider.get_remote_link image_path
           end
         else
-          File.join('/', app.images_dir, remote_images_dir, image_name)
+          File.join('/', app.config[:images_dir], remote_images_dir, image_name)
         end
       end
 
@@ -77,7 +78,7 @@ module Middleman
       end
 
       def self.create_images_dir!
-        img_dir = File.join(app.root, 'source', app.images_dir, remote_images_dir)
+        img_dir = File.join(app.root, 'source', app.config[:images_dir], remote_images_dir)
 
         Dir.mkdir(img_dir) unless Dir.exist?(img_dir)
       end
